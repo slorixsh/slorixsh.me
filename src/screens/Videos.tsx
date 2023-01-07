43ffb587b576b72ee31eeb0c231178c7d61778b7
sup! <78882365+slorixsh@users.noreply.github.com>
2023-01-07 11:32:19 +0300

    Update Videos.tsx

--- src/screens/Videos.tsx
+++ src/screens/Videos.tsx
@@ -27,11 +27,9 @@ const Presence = () => {
   }, [apiCall]);
 
   const getUrl = (pagetoken) => {
-    const pt =
-        typeof pagetoken === "undefined" ? "" : `&pageToken=${pagetoken}`,
       mykey = "AIzaSyD1TMHkAva9kGDGW3MX1v-Ccv8Vmo10xnA",
       playListID = "PLhHTmkVZGji6wJdQofSxcsyWYm0P6BeSY",
-      URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playListID}&key=${mykey}${pt}`;
+      URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLhHTmkVZGji6wJdQofSxcsyWYm0P6BeSY&key=AIzaSyD1TMHkAva9kGDGW3MX1v-Ccv8Vmo10xnA$`;
     return URL;
   };
 
